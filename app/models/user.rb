cf74833948b72581842cb9673528fb01d42a5b08
EDEN <game.love.meat.0412@icloud.com>
2025-01-30 14:46:18 +0900

    商品出品機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,6 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true,
                              format: { with: KATAKANA_REGEX, message: 'is invalid. Input full-width katakana characters' }
   validates :birthday, presence: true
+
+  has_many :items, dependent: :destroy
 end
