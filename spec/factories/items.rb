591f1dbbf9204e5b766adbba8c6645965cacb48f
EDEN <game.love.meat.0412@icloud.com>
2025-01-30 15:38:53 +0900

    単体テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'Test Item' }
+    description { 'This is a test item.' }
+    price { 1000 }
+    category_id { 2 }
+    condition_id { 2 }
+    delivery_fee_id { 2 }
+    prefecture_id { 2 }
+    shipping_days_id { 2 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/fixtures/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,95 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品出品' do
+    context '出品できるとき' do
+      it '全ての項目が正しく入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できないとき' do
+      it 'nameが空では出品できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Name can't be blank"
+      end
+
+      it 'descriptionが空では出品できない' do
+        @item.description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Description can't be blank"
+      end
+
+      it 'priceが空では出品できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price can't be blank"
+      end
+
+      it 'priceが300未満では出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price must be greater than or equal to 300'
+      end
+
+      it 'priceが9999999を超えると出品できない' do
+        @item.price = 10_000_000
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price must be less than or equal to 9999999'
+      end
+
+      it 'priceが半角数字でなければ出品できない' do
+        @item.price = '１０００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'Price is not a number'
+      end
+
+      it 'category_idが1では出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category can't be blank"
+      end
+
+      it 'condition_idが1では出品できない' do
+        @item.condition_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Condition can't be blank"
+      end
+
+      it 'delivery_fee_idが1では出品できない' do
+        @item.delivery_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Delivery fee can't be blank"
+      end
+
+      it 'prefecture_idが1では出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Prefecture can't be blank"
+      end
+
+      it 'shipping_days_idが1では出品できない' do
+        @item.shipping_days_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Shipping days can't be blank"
+      end
+
+      it 'imageが空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Image can't be blank"
+      end
+
+      it 'userが紐付いていなければ出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include 'User must exist'
+      end
+    end
+  end
 end
